764c4112554d5d248c4bb6a95720b85dc3751c9e
elvinacsy <101494986+elvinacsy@users.noreply.github.com>
2022-04-19 15:24:50 +0800

    Add files via upload
    google map feature mentioned in previous discussion. can use it for footer then save time hehe

---
+++ google map_footer.php
@@ -0,0 +1,16 @@
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-opacity w3-black w3-large">
+    <section class="block col-2">
+      <!-- Google Map -->
+      <h2 class="w3-wide w3-center">LOCATE US AT:</h2><br>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3353.846321732169!2d110.34577720551866!3d1.5181045164343008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31fba769bac3f1ef%3A0x781053584b1d7949!2s52%2C%20Jalan%20Stampin%2C%2093350%20Kuching%2C%20Sarawak!5e0!3m2!1sen!2smy!4v1601640210484!5m2!1sen!2smy" width="1000" height="450" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+      </iframe><br><br>
+      <div class="quote">
+          <h5>"Your Satisfication is Our Happiness"</h5>
+      </div>
+    </section>
+
+    <div class="copyright noDeco">
+      <span>&copy;<a href="mailto:101226975@swinburne.students.edu.my">Pinocone</a>&verbar; Last Updated: 19/04/2022<br></span>
+    </div>
+</footer>
